920d66166acd5dd2d95e518efef7370e21df528d
babinicz <babinicz@onet.pl>
2021-08-18 20:39:33 +0200

    zmiana tła w slider

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ const App = () => {
             </Container>
             <Container>
               <Slider />
+              <IntoShape />
             </Container>
           </Route>
           <Route path="/próg" component={Feature}>
